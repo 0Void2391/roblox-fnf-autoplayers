591238ef8f02f8ba4e22fe3589b0e24f6207bdc7
0_Void <106687374+0Void2391@users.noreply.github.com>
2022-06-26 20:03:59 +0200

    v2

---
+++ mmm autoplayer.lua
@@ -0,0 +1,10 @@
+_G.Controls = {
+    [4] = {Left = "D", Down = "F", Up = "J", Right = "K"}, --// 4K
+    [5] = {Left = "D", Down = "F", Up = "J", Right = "K", Space = "Space"}, --// 5K
+    [6] = {Left = "S", Up = "D", Right = "F", Left2 = "J", Down = "K", Right2 = "L"}, --// 6K
+    [7] = {Left = "S", Up = "D", Right = "F", Left2 = "J", Down = "K", Right2 = "L", Space = "Space"}, --// 7K
+    [8] = {Left = "A", Down = "S", Up = "D", Right = "F", Left2 = "J", Down2 = "K", Up2 = "L", Right2 = "Semicolon"}, --// 8K
+    [9] = {Left = "A", Down = "S", Up = "D", Right = "F", Space = "Space", Left2 = "J", Down2 = "K", Up2 = "L", Right2 = "Semicolon"} --// 9K
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0Void2391/roblox-fnf-autoplayers/main/mmm.lua"))()
